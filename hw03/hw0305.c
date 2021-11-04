a962b3d01eb925872f037ea3e60f33d8ff2f79bd
MakerTakala <dylan91011028@gmail.com>
2021-11-04 13:33:16 +0800

    hw0305 finish

--- hw03/hw0305.c
+++ hw03/hw0305.c
@@ -1,32 +1,55 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main(){
-    int64_t year[1000] = 0;
-    double temperature[1000] = 0;
-    int16_t sampleQuantity;
+    int64_t year[1000] = {0};
+    double temperature[1000] = {0};
+    int64_t sampleQuantity = 0, sumYear = 0;
+    double sumTempreature = 0;
 
+    //input necessary information
     while(true){
         printf("Please enter the year: ");
         scanf("%ld", &year[sampleQuantity]);
-        if(year == -1){
+        if(year[sampleQuantity] == -1){
             break;
         }
-        if(year <= 0){
+        if(year[sampleQuantity] <= 0){
+            printf("\nWrong Input!!!\n");
+            printf("----------------------------------------------------------\n\n");
             exit(0);
         }
         printf("Temperature: ");
         scanf("%lf", &temperature[sampleQuantity]);
+
+        sumYear += year[sampleQuantity];
+        sumTempreature += temperature[sampleQuantity];
+
         sampleQuantity++;
     }
 
+    if(sampleQuantity == 0){
+        printf("\nNot enough Input!!!\n");
+        printf("----------------------------------------------------------\n\n");
+        exit(0);
+    }
+
+    //calculate regression line
     double Sxy = 0, Sxx = 0;
-    double averageX = 0 averageX = 0;
+    double averageYear = sumYear / sampleQuantity, averageTemperature = sumTempreature / sampleQuantity;
+    for(int i = 0; i < sampleQuantity; i++){
+        double Xi = year[i] - averageYear , Yi = temperature[i] - averageTemperature;
+        Sxy += Xi * Yi;
+        Sxx += Xi * Xi;
+    }
+    long double slope = Sxy / Sxx, intercept = temperature[0] - slope * year[0];
 
     int64_t predictYear = 0;
     printf("Please enter the prediction year: ");
-    scanf("%ld", &year);
-
+    scanf("%ld", &predictYear);
+    double predictTemperature = predictYear * slope + intercept;
+    printf("Temperature: %Lf\n", predictTemperature);
     return 0;
 }
---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    double a = 23.1414;
+    int b = 2935;
+    double out = a / b;
+    printf("%lf", out);
+}
