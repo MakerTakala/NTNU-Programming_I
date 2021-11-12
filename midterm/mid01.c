4084a37f57828f0552eb8b6a53c47ca797dffde1
MakerTakala <dylan91011028@gmail.com>
2021-11-12 23:06:46 +0800

    update

--- midterm/mid01.c
+++ midterm/mid01.c
@@ -3,10 +3,6 @@
 #include "midfunction.h"
 
 int main(){
-    int x1 = 121, y1 = 3412, x2 = 1412, y2 = 124124;
-    point s1 = {x1, y1};
-    point s2 = {x2, y2};
-    line l1 = {s1, s2};
-    printf("%Lf", getSlope(l1) );
+    
     return 0;
 }
