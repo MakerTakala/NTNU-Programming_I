f8b8109dc27642147ec8998babce95bc74fe9089
MakerTakala <dylan91011028@gmail.com>
2021-11-12 19:59:53 +0800

    update

--- midterm/midfunction.c
+++ midterm/midfunction.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "midfunction.h"
 
 void errorMessageExit(char message[]){
     printf("\n%s\n", message);
@@ -11,4 +12,20 @@ void errorMessageExit(char message[]){
 void askInput(char message[], int64_t* input){
     printf("%s", message);
     scanf("%ld", input);
+}
+
+double get_slope(line l) {
+    if (l.p1.x == l.p2.x) return 0.0 / 0.0;
+    if (l.p1.y == l.p2.y) return 0;
+
+    return (l.p1.y - l.p2.y) / (l.p1.x - l.p2.x);
+}
+
+point get_intersection(line l1, line l2) {
+    point p;
+
+    p.x = ((l1.p1.x * l1.p2.y - l1.p1.y * l1.p2.x) * (l2.p1.x - l2.p2.x) - (l1.p1.x - l1.p2.x) * (l2.p1.x * l2.p2.y - l2.p1.y * l2.p2.x)) / ((l1.p1.x - l1.p2.x) * (l2.p1.y - l2.p2.y) - (l1.p1.y - l1.p2.y) * (l2.p1.x - l2.p2.x));
+    p.y = ((l1.p1.x * l1.p2.y - l1.p1.y * l1.p2.x) * (l2.p1.y - l2.p2.y) - (l1.p1.y - l1.p2.y) * (l2.p1.x * l2.p2.y - l2.p1.y * l2.p2.x)) / ((l1.p1.x - l1.p2.x) * (l2.p1.y - l2.p2.y) - (l1.p1.y - l1.p2.y) * (l2.p1.x - l2.p2.x));
+
+    return p;
 }
--- midterm/midfunction.h
+++ midterm/midfunction.h
@@ -3,7 +3,19 @@
 
 #include <stdint.h>
 
+typedef struct{
+    double x;
+    double y;
+}point;
+
+typedef struct{
+    point p1;
+    point p2;
+}line;
+
 void errorMessageExit(char []);
 void askInput(char [], int64_t* );
+double get_slope(line l);
+point get_intersection(line l1, line l2);
 
 #endif
