f629e2dafcdcddf2c2baa37ddda4ada274f2ec68
MakerTakala <dylan91011028@gmail.com>
2021-11-25 19:23:38 +0800

    hw0402-2 finish

--- hw04/hw04fuc.c
+++ hw04/hw04fuc.c
@@ -1,7 +1,9 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "hw04fuc.h"
 
+//general
 void errorMessageExit(char message[]){
     printf("\n%s\n", message);
     printf("----------------------------------------------------------\n\n");
@@ -13,6 +15,7 @@ void askInput(char message[], int64_t* input){
     scanf("%ld", input);
 }
 
+//hw0401
 void kprint(int64_t n){
     for(int i = n / 2; i > 0; i--){
         printf("*");
@@ -31,9 +34,10 @@ void kprint(int64_t n){
     }
 }
 
+//hw0402
 void hanoi_recur(int64_t n, int8_t now, int8_t next, int8_t temp, int64_t disk){
     if(n == 1){
-        printf("move disk %ld form %hhd to rod %hhd\n", disk, now, next);
+        printf("move disk %ld to rod %hhd\n", disk, next);
         return;
     }
     hanoi_recur(n - 1, now, temp, next, disk - 1);
@@ -42,9 +46,94 @@ void hanoi_recur(int64_t n, int8_t now, int8_t next, int8_t temp, int64_t disk){
 }
 
 void hanoi_loop(int64_t n){
+    rod rod1 = initRod(1, n, 1), rod2 = initRod(2, n, 0), rod3 = initRod(3, n, 0);
+    int64_t move = power(2, n) - 1;
+    if(n % 2 == 0){
+        for(int i = 0; i < move; i++){
+            if(i % 3 == 0){
+                transformDisk(&rod1, &rod3);
+            }
+            else if(i % 3 == 1){
+                transformDisk(&rod1, &rod2);
+            }
+            else if(i % 3 == 2){
+                transformDisk(&rod2, &rod3);
+            }
+        }
+    }
+    else{
+        for(int i = 0; i < move; i++){
+            if(i % 3 == 0){
+                transformDisk(&rod1, &rod2);
+            }
+            else if(i % 3 == 1){
+                transformDisk(&rod1, &rod3);
+            }
+            else if(i % 3 == 2){
+                transformDisk(&rod2, &rod3);
+            }
+        }
+    }
     
 }
 
+void transformDisk(rod* rodx, rod* rody){
+    if(rody -> top == -1){
+        printf("move disk %hhd to rod %hhd\n", rodx -> disk[rodx -> top], rody -> status);
+        rody -> disk[++(rody -> top)] = rodx -> disk[rodx -> top];
+        (rodx -> top)--;
+        return;
+    }
+    if(rodx -> top == -1){
+        printf("move disk %hhd to rod %hhd\n", rody -> disk[rody -> top], rodx -> status);
+        rodx -> disk[++(rodx -> top)] = rody -> disk[rody -> top];
+        (rody -> top)--;
+        return;
+    }
+    if( (rodx -> disk[rodx -> top]) < (rody -> disk[rody -> top]) ){
+        printf("move disk %hhd to rod %hhd\n", rodx -> disk[rodx -> top], rody -> status);
+        rody -> disk[++(rody -> top)] = rodx -> disk[rodx -> top];
+        (rodx -> top)--;
+        return;
+    }
+    else{
+        printf("move disk %hhd to rod %hhd\n", rody -> disk[rody -> top], rodx -> status);
+        rodx -> disk[++(rodx -> top)] = rody -> disk[rody -> top];
+        (rody -> top)--;
+        return;
+    }
+}
+
+rod initRod(int8_t s,int8_t n, int8_t stuff){
+    rod tmprod;
+    tmprod.status = s;
+    if(stuff == 1){
+        tmprod.top = n - 1;
+        for(int i = 1; i <= n; i++){
+            tmprod.disk[i - 1] = n - i + 1;
+        }
+    }
+    else{
+        tmprod.top = -1;
+    }
+    return tmprod;
+}
+
+int64_t power(int64_t base, int64_t times){
+    if(base == 0){
+        return 0;
+    }
+    if(times == 0){
+        return 0;
+    }
+    
+    for(int i = 1, tmpbase = base; i < times; i++){
+        base *= tmpbase;
+    }
+    return base;
+}
+
+//hw0404
 long double countResistance(int64_t r, int64_t n){
     long double ans = 2 * r;
     for(int i = 1; i < n; i++){
--- hw04/hw04fuc.h
+++ hw04/hw04fuc.h
@@ -6,5 +6,13 @@ void errorMessageExit(char []);
 void askInput(char [], int64_t* );
 void kprint(int64_t n);
 void hanoi_recur(int64_t, int8_t, int8_t, int8_t, int64_t);
+typedef struct{
+    int8_t status;
+    int8_t disk[20];
+    int8_t top;
+}rod;
 void hanoi_loop(int64_t);
-long double countResistance(int64_t , int64_t );
+rod initRod(int8_t, int8_t, int8_t);
+void transformDisk(rod*, rod*);
+int64_t power(int64_t, int64_t);
+long double countResistance(int64_t, int64_t);
