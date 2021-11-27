5ccc641b5afdadf0f514ada32f2c1bc089bc5456
MakerTakala <dylan91011028@gmail.com>
2021-11-27 16:48:30 +0800

    some bugs have been caught

--- hw04/hw0401.c
+++ hw04/hw0401.c
@@ -5,7 +5,7 @@
 int main(){
     int64_t n = 0;
     askInput("Please enter n (1-100, n must be odd): ", &n);
-    if(n < 1 || n % 2 == 0){
+    if(n < 1 || 100 < n || n % 2 == 0){
         errorMessageExit("Wrong Input!");
     }
     kprint(n);
--- hw04/hw0404.c
+++ hw04/hw0404.c
@@ -5,12 +5,12 @@
 int main(){
     int64_t resistance = 0;
     askInput("Please enter the resistance (1-100): ", &resistance);
-    if(resistance <= 0 || 100 <= resistance){
+    if(resistance < 1 || 100 < resistance){
         errorMessageExit("Wrong Input");
     }
     int64_t n = 0;
     askInput("Please enter n (1-100):", &n);
-    if(n <= 0 || 100 <= n){
+    if(n < 1 || 100 < n){
         errorMessageExit("Wrong Input");
     }
     printf("Ans: %Lf\n", countResistance(resistance, n));
